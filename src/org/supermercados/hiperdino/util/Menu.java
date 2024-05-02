28955b62af156805f53c69079aaae89e7a7fdb59
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 22:41:52 +0100

    feat(Menu): Método  mostrarMenuPrincipal() aregado a la clase.

---
+++ src/org/supermercados/hiperdino/util/Menu.java
@@ -0,0 +1,16 @@
+package org.supermercados.hiperdino.util;
+
+public class Menu {
+
+    public static void mostrarMenuPrincipal() {
+
+        System.out.print("""
+                  \nGestión de locales Hiperdino
+                Seleccione una opción. ¿Quién eres?
+                a. Cliente.
+                b. Cajero.
+                c. Salir del programa.
+                """);
+    }
+
+}
