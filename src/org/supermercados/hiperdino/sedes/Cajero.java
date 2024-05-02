3d022f7415caa891b8e6b6661093b68d08450a97
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 22:03:45 +0100

    feat(Cajero): Clase Cajero y atributos agregados

---
+++ src/org/supermercados/hiperdino/sedes/Cajero.java
@@ -0,0 +1,13 @@
+package org.supermercados.hiperdino.sedes;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Cajero {
+
+    private int numeroIdenticacion;
+    private Queue<Cliente> colaClientes = new LinkedList<>();
+    private boolean cajaAbierta = false;
+    private int clientesAtendidos = 0;
+
+}
