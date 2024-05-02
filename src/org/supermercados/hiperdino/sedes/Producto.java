701a9f45209065020b44104730c0207407686262
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 21:15:37 +0100

    feat(Producto): Métodos de acceso agregados a la clase

--- src/org/supermercados/hiperdino/sedes/Producto.java
+++ src/org/supermercados/hiperdino/sedes/Producto.java
@@ -11,4 +11,25 @@ public class Producto {
         this.precio = precio;
     }
 
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
 }
