968b35547f5f15ff124c622b8240f0f326cf454a
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 21:25:12 +0100

    feat(Cliente): Clase Cliente y atributos agregados

---
+++ src/org/supermercados/hiperdino/sedes/Cliente.java
@@ -0,0 +1,11 @@
+package org.supermercados.hiperdino.sedes;
+
+import java.util.Stack;
+
+public class Cliente {
+
+    private String nombre;
+    private String dni;
+    private Stack<Producto> cestaCompra = new Stack<>();;
+
+}
