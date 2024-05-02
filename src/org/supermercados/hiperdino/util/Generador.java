90d6f1e03c74e12c59242d86a6632a90d26b9a43
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 22:29:16 +0100

    feat: Método productoAleatorio()  de tipo Producto agregado a la clase

--- src/org/supermercados/hiperdino/util/Generador.java
+++ src/org/supermercados/hiperdino/util/Generador.java
@@ -1,5 +1,9 @@
 package org.supermercados.hiperdino.util;
 
+import java.util.Random;
+
+import org.supermercados.hiperdino.sedes.Producto;
+
 public class Generador {
 
     private static String[] personas = { "Antonio", "Belén", "Celeste", "Bartolomé", "Antonia", "Dolly", "Gerardo",
@@ -9,5 +13,19 @@ public class Generador {
     private static String[] productos = { "Tomate", "Huevo", "Leche", "Plátano Verde", "Sal", "Carne", "Pollo",
             "Azucar",
             "Arroz", "Jamón Serrano", "Pulpo", "Pera" };
+
+    public static Producto productoAleatorio() {
+
+        Random nombre = new Random();
+        int indiceRandom = nombre.nextInt(productos.length);
+
+        Random precio = new Random();
+
+        float precioRandom = (float) (Math.round((precio.nextFloat() * 15 + 1) * 100.0) / 100.0);
+
+        Producto producto = new Producto(productos[indiceRandom], precioRandom);
+        return producto;
+
+    }
 
 }
