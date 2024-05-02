0501e7f33e26752a4c1d429ae53d7c3f39639faf
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-02 22:27:25 +0100

    feat(Generador): Clase Generador agregada junto con arrays de personas y productos.

---
+++ src/org/supermercados/hiperdino/util/Generador.java
@@ -0,0 +1,13 @@
+package org.supermercados.hiperdino.util;
+
+public class Generador {
+
+    private static String[] personas = { "Antonio", "Belén", "Celeste", "Bartolomé", "Antonia", "Dolly", "Gerardo",
+            "Fernando", "Lorena", "Darwin", "Emily", "Luciana", "Raul", "Pepe", "Lucas", "Felipe", "Hilda", "Andrea",
+            "Samantha", "Alejandra" };
+
+    private static String[] productos = { "Tomate", "Huevo", "Leche", "Plátano Verde", "Sal", "Carne", "Pollo",
+            "Azucar",
+            "Arroz", "Jamón Serrano", "Pulpo", "Pera" };
+
+}
